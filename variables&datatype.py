2c1cbc2e3c68873016236783df33765e30ae7b78
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-03 12:02:06 -0700

    variables and datatypes introduction

---
+++ variables&datatype.py
@@ -0,0 +1,25 @@
+character_name = "Medhansh"
+character_age= 18 
+
+print("there once was a boy named " + character_name + ",")
+print("he was " + character_age + " years old ")
+
+
+
+# DATATYPES
+
+
+# 3 basic types of data we will be dealing with in python are 
+# numbers
+# strings
+# boleean (true or false )
+# note - there are bunch of other types of data but as a new python programmer you will using these 3 most of the time :)
+
+
+
+# VARIABLES 
+
+# variables are simply the conatiners of data 
+# you can give a variable name and store some kind of data in it 
+# variables are helpfull in managing the data throughout the program 
+# while giving name to a variable you separate two words with the help of _ 
