ebdfbcb788efb4c8740db62473094fd2e41e9306
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-05 11:49:47 +0530

    taking input from user

---
+++ input.py
@@ -0,0 +1,6 @@
+# Getting input from users 
+name = (input("Enter your name :- "))
+print (" hello " + name + " ! ") 
+
+# here we createda varible named name , and took an input from user and stored it into that varibale .
+# now while printing the message python will take the input from the variable   
