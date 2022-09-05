85948763d570001195a8fd1e561275e8823bc17b
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-05 11:33:51 +0530

    working with numbers in python

---
+++ numbers.py
@@ -0,0 +1,63 @@
+# Number datatype is also a very commonly used datatype in python
+# we can do multiple arethmatic operationns using numbers 
+
+# addition
+ 
+print(2+3) 
+
+# for substraction (-) , for multiplication (*), for division (/) 
+# we can also manupulate the fllow oftheoperation 
+# for example 
+
+print(3 * 4 +5 ) # this will give output of 17 
+
+print(3* (4+5)) # this will give the output of 27 as it will add first and then multiply 
+
+# modulus operation 
+
+print( 10 % 3 ) # it is read as 10 mod (%) 3 , basically it means then first number divided by second number 
+# it will give remainder as output 
+
+# you can also store numbers inside a variable 
+
+num = 3
+print(num) 
+
+# Apart from basic arethematic we can do some other stuff as well using numbers 
+
+# converting nuber into string 
+
+print(str(num)) # it is helpful when we want to print a number alongside a string 
+# example 
+print(str(num) + " is a one digit one ") 
+
+# Functions on number 
+
+# abs stands for absolute value of a number 
+
+num2= -3
+print(abs(num2)) 
+
+#pow 
+print(pow(3 ,2 )) # it will give 3 ^ 2s
+
+# max 
+print(max(5,6)) # it will give the maximum number outof the two numbers
+
+# min 
+print(min(5,6)) # gives minimum value 
+
+# round 
+
+print(round(3.9)) # it will round the number 
+
+# In Python youcanimport functions in order to do specific math task 
+from math import * 
+
+print (floor(3.7)) # it will give the minimum value , chop off the decimal values 
+
+# square root 
+
+print(sqrt(8))  # it will give the square root of the number 
+
+# NOTE :- THERE ARE MULTIPLE FUNCTIONS AND OPERATIONS YOU CAN PERFORM WITH NUMBERS, YOU CAN GOGGLE THE  LIST :) 
