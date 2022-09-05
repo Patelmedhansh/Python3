c4e63ce2c275c47204b7454ebad1fdc284dd67e5
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-05 12:00:05 +0530

    basic calculator using python

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# Bulding basic calculator 
+num1 = int (input ("Enter a number :- ") )
+num2  = int (input ("Enter a number :- ") )  
+
+result = num1 + num2
+
+print(result) 
+
+# Here when we take input from a user , by default python converts it into string 
+# so here we need to define it as int ( integer ) 
+
+# when you use int function it only accepts the whole numbers , so instead of int you can use float function as well 
+# if you want to enter decimal  values 
