0ac27b81df52b4aeb41d59d8d85c2d3073c8efc0
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-05 12:10:25 +0530

    mad libs game using python

---
+++ game.py
@@ -0,0 +1,8 @@
+# Building mad libs game using python 
+color = input("enter a color ") 
+noun = input("enter a noun ") 
+celebrity = input("enter your celebrity crush ") 
+
+print(" Flowers are " + color ) 
+print( noun +  "are blue ") 
+print("i love " + celebrity ) 
