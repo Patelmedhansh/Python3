869a1d0c441aaffda2023283c5c3e944437bcf12
MeDzZ <Medhanshpatel18@gmail.com>
2022-09-09 12:15:04 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,1 +1,9 @@
 # Working with tuples in pyhton 
+
+# A tuple is a type of data structure , means it is a type of container 
+# a tuple is very similar to a list , with some key differences 
+
+coordinates = (1,2) 
+# A tuple is immutable , which means it can not be changes or modified 
+
+print(coordinates[0]) 
